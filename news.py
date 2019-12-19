39f3a5e6a003caf7f4fe8a3a7d5f7fdf77bf0105
chickenugget0218 <chickenugget0218@gmail.com>
2019-12-19 22:58:49 +0900

    리스트수정함

--- news.py
+++ news.py
@@ -4,13 +4,21 @@ import requests
 import pandas as pd
 import re
 
+#크롤링 결과 임시 저장 리스트 선언
+t_text=[]
+l_text=[]
+s_text=[]
+d_text=[]
+c_text=[]
+
 #각 크롤링 결과 저장하기 위한 리스트 선언
 title_text=[]
 link_text=[]
 source_text=[]
 date_text=[]
 contents_text=[]
-result={}
+#result={}
+result=[]
 
 #엑셀로 저장하기 위한 변수
 RESULT_PATH='C:/2019_2/crawling_result/'
@@ -43,7 +51,9 @@ def contents_cleansing(contents):
     third_cleansing_contents = re.sub('<.+?','',second_cleansing_contents).strip()
     for_cleansing_contents = re.sub('trong class="hl">','',third_cleansing_contents).strip()
     fifth_cleansing_contents = re.sub('strong>','',for_cleansing_contents).strip()
-    contents_text.append(fifth_cleansing_contents)
+
+    six_cleansing_contents = re.sub( 'dd> dl>','',fifth_cleansing_contents).strip()
+    contents_text.append(six_cleansing_contents)
 
 
 #크롤러
@@ -61,11 +71,6 @@ def crawler(maxpage,query,sort,s_date,e_date):
         response =requests.get(url)
         html=response.text
 
-        #beautifulsoup의 인자값 지정=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
-
-        response = requests.get(url)
-        html = response.text
-
         #뷰티풀소프의 인자값 지정
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -74,17 +79,22 @@ def crawler(maxpage,query,sort,s_date,e_date):
         for atag in atags:
             title_text.append(atag.text)     #제목
             link_text.append(atag['href'])   #링크주소
+            t_text.append(title_text) #추가한부분
+            l_text.append(link_text)
+
 
         #신문사 추출
         source_lists = soup.select('._sp_each_source')
         for source_list in source_lists:
             source_text.append(source_list.text)    #신문사
+            s_text.append(source_text)
 
         #날짜 추출
         date_lists = soup.select('.txt_inline')
         for date_list in date_lists:
             test=date_list.text
             date_cleansing(test)  #날짜 정제 함수사용
+            d_text.append(date_text)
 
         #본문요약본
         contents_lists = soup.select('ul.type01 dl')
@@ -92,37 +102,47 @@ def crawler(maxpage,query,sort,s_date,e_date):
             #print('==='*40)
             #print(contents_list)
             contents_cleansing(contents_list) #본문요약 정제화
+            c_text.append(contents_text)
 
 
         #모든 리스트 딕셔너리형태로 저장
-        result= {"date" : date_text , "title":title_text ,  "source" : source_text ,"contents": contents_text ,"link":link_text }
+        #result= {"date" : date_text , "title":title_text ,  "source" : source_text ,"contents": contents_text ,"link":link_text }
+        #딕셔너리형태를 리스트로 지정
+
+        #페이지출력, 주석처리가능
         print(page)
 
-        df = pd.DataFrame(result)  #df로 변환
+        #문자열 리스트로 만든다
+        #final = [date_text,title_text,source_text,contents_text,link_text]
+        #ff=final
+
+        #df = pd.DataFrame(result)  #df로 변환
         page += 10
 
 
     # 새로 만들 파일이름 지정
-    outputFileName = '%s-%s-%s  %s시 %s분 %s초.csv' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
-    df.to_csv(RESULT_PATH+outputFileName,encoding='utf-8-sig')
+    #outputFileName = '%s-%s-%s  %s시 %s분 %s초.csv' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    #df.to_csv(RESULT_PATH+outputFileName,encoding='utf-8-sig')
     #dataframe을 csv로 저장
 
 
 def main():
-    #info_main = input("="*50+"\n"+"입력 형식에 맞게 입력해주세요"+"\n"+"시작하시려면 Enter를 눌러주세요."+"\n"+"="*50)
 
-#    maxpage = input("최대 크롤링할 페이지 수 입력하세요: ")
-    maxpage = '100' #최대 100페이지 크롤링
+    #크롤링할 페이지 수
+    # 1페이지는 1, 2페이지는 11 , 3페이지는 21, 4페이지는 31
+    maxpage ='5' #최대 100페이지 크롤링, 여기 숫자수정하면 됨
 
-    #범죄 키워드 리스트, 여기를 수정가능
+    #범죄 키워드 리스트, 키워드 수정가능
     crime = ['음주운전','탈세','성폭행','구속','마약','처벌','혐의']
-    #리스트를 문자열로 변환
+
+    #crime 리스트를 문자열로 변환
     res=(' '.join(crime))
 
     #이부분 수정되어야함, 가수이름 문자열로 넘겨받음
     query=input("검색어 입력: ")
+
     #검색키워드+문자열
-    query+res
+    query=query+res
 
  #   sort=input("뉴스 검색 방식 입력(관련도순0/최신수1/오래된순2): ")
     sort='0' #관련도순으로 크롤링
@@ -134,5 +154,29 @@ def main():
     e_date='2019.12.19'
     # 크롤러 실행
     crawler(maxpage,query,sort,s_date,e_date)
+
+   #결과값 담는 리스트
+    result2 = []
+    rdate=[]
+    rtitle=[]
+    rcontent=[]
+    rlink=[]
+
+    #for문 돌려서 리스트에 추가
+    for j in range(len(date_text)):
+        for i in range(len(date_text)):
+            rdate.append(date_text[i])
+        for i in range(len(date_text)):
+            rtitle.append(title_text[i])
+        for i in range(len(date_text)):
+            rcontent.append(contents_text[i])
+        for i in range(len(date_text)):
+            rlink.append(date_text[i])
+        for i in range(len(date_text)):
+            result=[rdate[i],rtitle[i],rcontent[i],rlink[i]]
+            result2.append(result)
+    print(result2) #print
+
+
 
 main()
